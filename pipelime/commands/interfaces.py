85bb4224606784bb6ed9b241d6eb0f9c609a00ec
nfioraio-ec <nicola.fioraio@eyecan.ai>
2022-05-17 15:43:15 +0200

    Sample schema validation interface

--- pipelime/commands/interfaces.py
+++ pipelime/commands/interfaces.py
@@ -41,6 +41,102 @@ class GrabberInterface(pyd.BaseModel, extra="forbid"):
         grab_all(grabber, sequence, track_fn=track_fn, sample_fn=sample_fn)
 
 
+class SampleValidationInterface(pyd.BaseModel, extra="forbid"):
+    """Sample schema validation."""
+
+    sample_schema: t.Union[str, t.Mapping[str, str]] = pyd.Field(
+        ...,
+        description=(
+            "The sample schema to validate, ie, a mapping from sample keys to expected "
+            "item types.\nThe schema can be a class path to a pydantic model where "
+            "fields' names are the sample keys, while fields' values are the item "
+            "types. Otherwise, an explicit `key: item-classpath` mapping must be "
+            "provided."
+        ),
+    )
+    validators: t.Optional[t.Mapping[str, str]] = pyd.Field(
+        None,
+        description=(
+            "When `sample_schema` is an explicit mapping, validators "
+            "can be set for each field as a class path to a callable accepting the "
+            "field value and either returning a validated version or raising an "
+            "exception in case of error."
+        ),
+    )
+    ignore_extra_keys: bool = pyd.Field(
+        True,
+        description=(
+            "When `sample_schema` is an explicit mapping, if `ignore_extra_keys` is "
+            "True, unexpected keys are ignored. Otherwise an error is raised."
+        ),
+    )
+    lazy: bool = pyd.Field(
+        True, description="If True, samples will be validated only when accessed."
+    )
+    max_samples: int = pyd.Field(
+        -1,
+        description=(
+            "When the validation is NOT lazy, "
+            "only the slice `[0:max_samples]` is checked."
+        ),
+    )
+
+    _schema_model: t.Type[pyd.BaseModel] = pyd.PrivateAttr(pyd.BaseModel)
+
+    def _make_pydantic_schema(self):
+        from pipelime.choixe.utils.imports import import_symbol
+        from pipelime.items import Item
+
+        if isinstance(self.sample_schema, str):
+            imported_v = import_symbol(self.sample_schema)
+            if not issubclass(imported_v, pyd.BaseModel):
+                raise ValueError(f"`{self.sample_schema}` is not a pydantic model.")
+            self._schema_model = imported_v
+        elif isinstance(self.sample_schema, t.Mapping):
+            item_map = {
+                k: import_symbol(cp if "." in cp else f"pipelime.items.{cp}")
+                for k, cp in self.sample_schema.items()
+            }
+            for item_cls in item_map.values():
+                if not issubclass(item_cls, Item):
+                    raise ValueError(
+                        "Model mapping must be a `key: item-class` mapping."
+                    )
+
+            class Config(pyd.BaseConfig):
+                arbitrary_types_allowed = True
+                extra = pyd.Extra.ignore if self.ignore_extra_keys else pyd.Extra.forbid
+
+            _validators = None
+            if self.validators is not None:
+                _validators = {}
+                for k, v_cp in self.validators.items():
+                    v_call = import_symbol(v_cp)
+
+                    def _validator_wrapper(cls, val):
+                        return v_call(val)
+
+                    _validators[f"validate_{k}"] = pyd.validator(k)(_validator_wrapper)
+
+            self._schema_model = pyd.create_model(
+                "SampleSchema",
+                __config__=Config,
+                __validators__=_validators,  # type: ignore
+                **item_map,
+            )
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self._make_pydantic_schema()
+
+    def append_validator(self, sequence):
+        return sequence.validate_samples(
+            sample_schema=self._schema_model,
+            lazy=self.lazy,
+            max_samples=self.max_samples,
+        )
+
+
 class InputDatasetInterface(pyd.BaseModel, extra="forbid"):
     """Input dataset options."""
 
@@ -52,13 +148,19 @@ class InputDatasetInterface(pyd.BaseModel, extra="forbid"):
             "to each sample (sample values take precedence)."
         ),
     )
+    schema_: t.Optional[SampleValidationInterface] = pyd.Field(
+        None, alias="schema", description="Sample schema validation."
+    )
 
     def create_reader(self):
         from pipelime.sequences import SamplesSequence
 
-        return SamplesSequence.from_underfolder(  # type: ignore
+        reader = SamplesSequence.from_underfolder(  # type: ignore
             folder=self.folder, merge_root_items=self.merge_root_items, must_exist=True
         )
+        if self.schema_ is not None:
+            reader = self.schema_.append_validator(reader)
+        return reader
 
     def __str__(self) -> str:
         return str(self.folder)
@@ -148,17 +250,23 @@ class OutputDatasetInterface(pyd.BaseModel, extra="forbid"):
         default_factory=SerializationModeInterface,
         description="Serialization modes for items and keys.",
     )
+    schema_: t.Optional[SampleValidationInterface] = pyd.Field(
+        None, alias="schema", description="Sample schema validation."
+    )
 
     def serialization_cm(self) -> t.ContextManager:
         return self.serialization
 
     def append_writer(self, sequence):
-        return sequence.to_underfolder(
+        writer = sequence.to_underfolder(
             folder=self.folder,
             zfill=self.zfill,
             exists_ok=self.exists_ok,
             key_serialization_mode=self.serialization.keys,
         )
+        if self.schema_ is not None:
+            writer = self.schema_.append_validator(writer)
+        return writer
 
     def __str__(self) -> str:
         return str(self.folder)
--- pipelime/sequences/pipes/schema.py
+++ pipelime/sequences/pipes/schema.py
@@ -1,9 +1,9 @@
+import typing as t
+
 import pydantic as pyd
 
 import pipelime.sequences as pls
-from pipelime.items import Item
 from pipelime.sequences.pipes.base import PipedSequenceBase
-import typing as t
 
 
 @pls.piped_sequence
@@ -12,29 +12,11 @@ class ValidatedSequence(
 ):
     """Validates the source sequence against a schema."""
 
-    sample_schema: t.Union[
-        str, t.Type[pyd.BaseModel], t.Mapping[str, t.Union[str, t.Type[Item]]]
-    ] = pyd.Field(
+    sample_schema: t.Type[pyd.BaseModel] = pyd.Field(
         ...,
         description=(
             "The sample schema to validate, ie, a pydantic model where fields name are "
-            "the sample keys and field types are the expected item classes. The model "
-            "can be a pydantic model class or an explicit `key: item-class` mapping."
-        ),
-    )
-    validators: t.Optional[t.Mapping[str, t.Union[str, t.Callable]]] = pyd.Field(
-        None,
-        description=(
-            "When `sample_schema` is an explicit mapping, validators "
-            "can be set for each field as a simple callable accepting the field value "
-            "and returning a validated version, while raising exception in case of "
-            "error."
-        ),
-    )
-    ignore_extra_keys: bool = pyd.Field(
-        True,
-        description=(
-            "If True, extra keys are ignored, otherwise their presence raises an error."
+            "the sample keys and field types are the expected item classes."
         ),
     )
     lazy: bool = pyd.Field(
@@ -48,67 +30,8 @@ class ValidatedSequence(
         ),
     )
 
-    @pyd.validator("sample_schema")
-    def import_schema(cls, v):
-        from pipelime.choixe.utils.imports import import_symbol
-
-        if isinstance(v, str):
-            imported_v = import_symbol(v)
-            if not issubclass(imported_v, pyd.BaseModel):
-                raise ValueError(f"`{v}` is not a pydantic model.")
-            v = imported_v
-        elif isinstance(v, t.Mapping):
-            v = {
-                k: import_symbol(val) if isinstance(val, str) else val
-                for k, val in v.items()
-            }
-            for k, val in v.items():
-                if not isinstance(k, str) or not issubclass(val, Item):
-                    raise ValueError(
-                        "Model mapping must be a `key: item-class` mapping."
-                    )
-        else:
-            if not issubclass(v, pyd.BaseModel):
-                raise ValueError(f"`{v}` is not a pydantic model.")
-        return v
-
-    @pyd.validator("validators")
-    def import_validators(cls, v):
-        from pipelime.choixe.utils.imports import import_symbol
-
-        if v is None:
-            return None
-        return {
-            k: import_symbol(val) if isinstance(val, str) else val
-            for k, val in v.items()
-        }
-
     def __init__(self, **data):
         super().__init__(**data)
-        if isinstance(self.sample_schema, t.Mapping):
-            import pydantic as pyd
-
-            class Config(pyd.BaseConfig):
-                arbitrary_types_allowed = True
-                extra = pyd.Extra.ignore if self.ignore_extra_keys else pyd.Extra.forbid
-
-            _validators = None
-            if self.validators is not None:
-                _validators = {}
-                for k, v in self.validators.items():
-
-                    def _validator(cls, val):
-                        return v(val)  # type: ignore
-
-                    _validators[f"{k}_validator"] = pyd.validator(k)(_validator)
-
-            self.sample_schema = pyd.create_model(
-                "SampleSchema",
-                __config__=Config,
-                __validators__=_validators,  # type: ignore
-                **self.sample_schema,  # type: ignore
-            )
-
         if not self.lazy:
             for sample in self.source[0 : self.max_samples]:  # noqa: E203
                 self._check_sample(sample)  # type: ignore
@@ -120,5 +43,14 @@ class ValidatedSequence(
         return sample
 
     def _check_sample(self, sample: pls.Sample):
-        # Throws if validation fails
-        _ = self.sample_schema(**sample)  # type: ignore
+        from pydantic import ValidationError
+        from pydantic.error_wrappers import display_errors
+
+        try:
+            _ = self.sample_schema(**sample)  # type: ignore
+        except ValidationError as e:
+            errs = e.errors()
+            raise ValueError(
+                f"Sample schema validation failed for:\n{str(sample)}\n\n"
+                f"Errors:\n{display_errors(errs)}"
+            )
