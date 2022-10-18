391cd4eac8b0b138d65a3e3505c1d89c069d0f60
LuccBonfiglioli <luca.bonfiglioli@gmail.com>
2022-10-18 21:55:14 +0200

    Bugfix, ITS working on all tested cases

--- pipelime/choixe/utils/rand.py
+++ pipelime/choixe/utils/rand.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from statistics import fmean
 from typing import Any, Callable, Mapping, Optional, Sequence, Tuple, TypeVar, Union
 
 import numpy as np
+
 
 T = TypeVar("T", float, Sequence[float], np.ndarray)
 
@@ -33,8 +35,18 @@ class RealFn(ABC):
         pass
 
     @abstractmethod
+    def shift(self, dx: float, dy: float) -> RealFn:
+        pass
+
+    @abstractmethod
     def _repr(self) -> str:
         pass
+
+
+t_piece_fn = Callable[[np.ndarray], np.ndarray]
+t_piece = Union[float, Tuple[float, float], t_piece_fn, RealFn]
+t_piecewise = Union[Sequence[t_piece], Mapping[float, t_piece]]
+t_pdf = Union[RealFn, t_piece_fn, Sequence[t_piece], Mapping[float, t_piece]]
 
 
 class LinearFn(RealFn):
@@ -55,6 +67,9 @@ class LinearFn(RealFn):
         if self.m == 0:
             return IdentityFn()
         return LinearFn(1 / self.m, -self.q / self.m)
+
+    def shift(self, dx: float, dy: float) -> LinearFn:
+        return LinearFn(self.m, self.q + dy - self.m * dx)
 
     def _repr(self) -> str:
         return f"{round(self.m, 2)}x + {round(self.q, 2)}"
@@ -81,6 +96,9 @@ class ConstantFn(LinearFn):
     def integrate(self) -> RealFn:
         return LinearFn(self.q, 0)
 
+    def shift(self, dx: float, dy: float) -> ConstantFn:
+        return ConstantFn(self.q + dy)
+
 
 class QuadraticFn(RealFn):
     def __init__(self, a: float, b: float, c: float) -> None:
@@ -96,89 +114,119 @@ class QuadraticFn(RealFn):
         raise NotImplementedError("Bruh")
 
     def invert(self, start: float, stop: float) -> Optional[RealFn]:
-        a, b, c = self.a, self.b, self.c
+        a, b, c, eps = self.a, self.b, self.c, 1e-6
         if a == 0:
             return LinearFn(b, c).invert(start, stop)
         vertex_x = -b / (2 * a)
-        if start < vertex_x < stop:
+        print(vertex_x, start, stop)
+        if start + eps < vertex_x < stop - eps:
             return None
-        sv = -1 if start < vertex_x else 1
+        sv = -1 if (start + stop) / 2 < vertex_x else 1
         sa = -1 if a < 0 else 1
+        p, q = self(np.array([start, stop])).tolist()
         return GenericFn(
             lambda x: (
                 -b + sv * sa * np.sqrt(np.clip(b**2 - 4 * a * (c - x), 0, None))
             )
-            / (2 * a)
+            / (2 * a),
+            min(p, q),
+            max(p, q),
         )
+
+    def shift(self, dx: float, dy: float) -> QuadraticFn:
+        a = self.a
+        b = self.b - 2 * a * dx
+        c = a * dx**2 - b * dx + self.c + dy
+        return QuadraticFn(a, b, c)
 
     def _repr(self) -> str:
         return f"{round(self.a, 2)}x² + {round(self.b, 2)}x + {round(self.c, 2)}"
 
 
-class ShiftedFn(RealFn):
-    def __init__(self, fn: RealFn, dx: float, dy: float) -> None:
-        super().__init__()
-        self.fn = fn
-        self.dx = dx
-        self.dy = dy
-
-    def _call(self, x: np.ndarray) -> np.ndarray:
-        return self.fn._call(x - self.dx) + self.dy
-
-    def integrate(self) -> RealFn:
-        return GenericFn(lambda x: self.fn.integrate()(x) + self.dy * x)
-
-    def invert(self, start: float, stop: float) -> Optional[RealFn]:
-        inverse = self.fn.invert(start - self.dx, stop - self.dx)
-        return ShiftedFn(inverse, self.dy, self.dx) if inverse else None
-
-    def _repr(self) -> str:
-        return f"g(x - {round(self.dx, 2)}) + {round(self.dy, 2)}"
-
-
-class ScaledFn(RealFn):
-    def __init__(self, fn: RealFn, sx: float, sy: float) -> None:
-        super().__init__()
-        self.fn = fn
-        self.sx = sx
-        self.sy = sy
-
-    def _call(self, x: np.ndarray) -> np.ndarray:
-        return self.fn(x / self.sx) * self.sy
-
-    def integrate(self) -> RealFn:
-        return ScaledFn(self.fn.integrate(), self.sx, self.sy)
-
-    def invert(self, start: float, stop: float) -> Optional[RealFn]:
-        inverse = self.fn.invert(start / self.sx, stop / self.sx)
-        return ScaledFn(inverse, self.sy, self.sx) if inverse else None
-
-    def _repr(self) -> str:
-        return f"{round(self.sy, 2)}g({round(1/self.sx, 2)}x)"
-
-
 class GenericFn(RealFn):
     def __init__(
-        self, fn: Callable[[np.ndarray], np.ndarray], steps: int = 100
+        self,
+        fn: Callable[[np.ndarray], np.ndarray],
+        start: float,
+        stop: float,
+        steps: int = 100,
     ) -> None:
         super().__init__()
         self.fn = fn
+        self.start = start
+        self.stop = stop
         self.steps = steps
 
     def _call(self, x: np.ndarray) -> np.ndarray:
         return self.fn(x)
 
+    def _to_piecewise(self) -> PiecewiseFn:
+        x = np.linspace(self.start, self.stop, self.steps + 1)
+        y = self(x)
+        x, y = x.tolist(), y.tolist()
+        segments = {x[i]: (y[i], y[i + 1]) for i in range(len(x) - 1)}
+        return PiecewiseFn.parse(segments, self.start, self.stop)
+
     def integrate(self) -> PiecewiseFn:
-        raise NotImplementedError("Bruh")
+        return self._to_piecewise().integrate()
 
     def invert(self, start: float, stop: float) -> Optional[PiecewiseFn]:
-        raise NotImplementedError("Bruh")
+        return self._to_piecewise().invert(start, stop)
+
+    def shift(self, dx: float, dy: float) -> GenericFn:
+        return GenericFn(lambda x: self(x - dx) + dy, self.start - dx, self.stop - dx)
 
     def _repr(self) -> str:
         return "code"
 
 
 class PiecewiseFn(RealFn):
+    @classmethod
+    def parse_piece(cls, piece: t_piece, start: float, stop: float) -> RealFn:
+        if isinstance(piece, RealFn):
+            return piece
+        elif isinstance(piece, float):
+            return ConstantFn(piece)
+        elif isinstance(piece, tuple):
+            m = (piece[1] - piece[0]) / (stop - start)
+            q = piece[0] - m * start
+            return LinearFn(m, q)
+        elif callable(piece):
+            return GenericFn(piece, start, stop)
+        else:
+            raise TypeError("Piece must be a parsable function")
+
+    @classmethod
+    def parse(
+        cls,
+        fn: t_piecewise,
+        start: Optional[float] = None,
+        stop: Optional[float] = None,
+    ) -> PiecewiseFn:
+        if isinstance(fn, Sequence):
+            segments = {}
+            start_, stop_ = start or 0.0, stop or 1.0
+            cur = start_
+            step = (stop_ - start_) / len(fn)
+            for piece in fn:
+                segments[cur] = cls.parse_piece(piece, cur, cur + step)
+                cur += step
+            segments[stop_] = ConstantFn(0.0)
+        elif isinstance(fn, Mapping):
+            segments = {}
+            keysteps = sorted(list(fn.keys()))
+            start_, stop_ = start or keysteps[0], stop or keysteps[-1]
+            for i in range(len(keysteps)):
+                cur, nxt = (
+                    keysteps[i],
+                    keysteps[i + 1] if i < len(keysteps) - 1 else stop_,
+                )
+                segments[cur] = cls.parse_piece(fn[cur], cur, nxt)
+            segments[stop_] = ConstantFn(0.0)
+        else:
+            raise TypeError("Invalid function")
+        return cls(segments)
+
     def __init__(self, segments: Mapping[float, RealFn]) -> None:
         self.segments = segments
         self.keysteps = sorted(list(segments.keys()))
@@ -201,7 +249,7 @@ class PiecewiseFn(RealFn):
             )
             integral = self.segments[cur].integrate()
             c0 = cumsum - integral(cur)
-            segments[cur] = ShiftedFn(integral, 0, c0)
+            segments[cur] = integral.shift(0, c0)
             cumsum += integral(nxt) - integral(cur)
 
         segments[float("inf")] = ConstantFn(cumsum)
@@ -224,6 +272,9 @@ class PiecewiseFn(RealFn):
                 segments[last] = inverted
                 start = nxt
         return PiecewiseFn(segments)
+
+    def shift(self, dx: float, dy: float) -> RealFn:
+        return PiecewiseFn({k - dx: v.shift(dx, dy) for k, v in self.segments.items()})
 
     def _repr(self) -> str:
         return "piecewise"
@@ -253,14 +304,9 @@ def plot(start: float, stop: float, *fn: Optional[RealFn], steps: int = 1000) ->
     plt.grid()
     plt.axhline(y=0, color="k")
     plt.axvline(x=0, color="k")
-    plt.axis("equal")
+    # plt.axis("equal")
     plt.xlim(start, stop)
     plt.show()
-
-
-t_piece_fn = Callable[[np.ndarray], np.ndarray]
-t_piece = Union[float, Tuple[float, float], t_piece_fn, RealFn]
-t_pdf = Union[RealFn, t_piece_fn, Sequence[t_piece], Mapping[float, t_piece]]
 
 
 class Distribution:
@@ -272,25 +318,14 @@ class Distribution:
         inverse_primitive = primitive.invert(start, stop)
         sum_ = primitive(stop)
 
-        self.pdf = lambda x: 0.0 if x < start or x >= stop else fn(x) / sum_
+        self.pdf = lambda x: fn(np.clip(x, start, stop)) / sum_
         self.cdf = lambda x: primitive(x) / sum_
         self.inverse_cdf = lambda x: inverse_primitive(x * sum_)  # type: ignore
 
+        plot(start, stop, self.pdf, self.cdf, steps=1000)  # type: ignore
+
     def sample_n(self, n: int) -> np.ndarray:
         return self.inverse_cdf(np.random.rand(n))
-
-    @classmethod
-    def _parse_piece(cls, piece: t_piece, start: float, stop: float) -> RealFn:
-        if isinstance(piece, RealFn):
-            return piece
-        elif isinstance(piece, float):
-            return ConstantFn(piece)
-        elif isinstance(piece, tuple):
-            m = (piece[1] - piece[0]) / (stop - start)
-            q = piece[0] - m * start
-            return LinearFn(m, q)
-        elif callable(piece):
-            return GenericFn(piece)
 
     @classmethod
     def parse(
@@ -300,30 +335,12 @@ class Distribution:
             return cls(fn, start or -float("inf"), stop or float("inf"))
         elif callable(fn):
             start_, stop_ = start or -float("inf"), stop or float("inf")
-            rfn = cls._parse_piece(fn, start_, stop_)  # type: ignore
+            rfn = PiecewiseFn.parse_piece(fn, start_, stop_)  # type: ignore
             return cls(rfn, start_, stop_)
-        elif isinstance(fn, Sequence):
-            segments = {}
-            start_, stop_ = start or 0.0, stop or 1.0
-            cur = start_
-            step = (stop_ - start_) / len(fn)
-            for piece in fn:
-                segments[cur] = cls._parse_piece(piece, cur, cur + step)
-                cur += step
-            segments[stop_] = ConstantFn(0.0)
-            rfn = PiecewiseFn(segments)
-            return cls(rfn, start_, stop_)
-        elif isinstance(fn, Mapping):
-            segments = {}
-            keysteps = sorted(list(fn.keys()))
-            start_, stop_ = start or keysteps[0], stop or keysteps[-1]
-            for i in range(len(keysteps)):
-                cur, nxt = (
-                    keysteps[i],
-                    keysteps[i + 1] if i < len(keysteps) - 1 else stop_,
-                )
-                segments[cur] = cls._parse_piece(fn[cur], cur, nxt)
-            rfn = PiecewiseFn(segments)
+        elif isinstance(fn, (Sequence, Mapping)):
+            rfn = PiecewiseFn.parse(fn)
+            keysteps = sorted(list(rfn.segments.keys()))
+            start_, stop_ = keysteps[0], keysteps[-1]
             return cls(rfn, start_, stop_)
         else:
             raise ValueError("Invalid distribution")
@@ -380,19 +397,21 @@ def rand(*args, n: int = 0, pdf: Optional[t_pdf] = None) -> Any:
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
+    n = int(1e7)
+
     pdf = [0.1, (1.0, 0.4), 2.0, (0.0, 0.2)]
-    samples = rand(0.0, 10.0, n=100000, pdf=pdf)
+    samples = rand(0.0, 10.0, n=n, pdf=pdf)
     plt.hist(samples, bins=100), plt.show()  # type: ignore
 
     pdf = [0.1, 1.0, 2.0, 0.2]
-    samples = rand(0.0, 10.0, n=100000, pdf=pdf)
+    samples = rand(0.0, 10.0, n=n, pdf=pdf)
     plt.hist(samples, bins=100), plt.show()  # type: ignore
 
-    pdf = {0.0: 0.1, 1.0: 1.0, 2.0: 2.0, 3.0: 0.2}
-    samples = rand(0.0, 10.0, n=100000, pdf=pdf)
+    pdf = {0.0: 0.1, 1.0: 1.0, 2.0: 2.0, 3.0: 0.2, 15.0: 120.0}
+    samples = rand(0.0, 10.0, n=n, pdf=pdf)
     plt.hist(samples, bins=100), plt.show()  # type: ignore
 
-    # # Gaussian pdf
-    # pdf = lambda x: np.exp(-(x**2) / 2) / np.sqrt(2 * np.pi)
-    # samples = rand(-5.0, 10.0, n=100000, pdf=pdf)
-    # plt.hist(samples, bins=100), plt.show()  # type: ignore
+    # Gaussian pdf
+    pdf = lambda x: np.exp(-(x**2) / 2) / np.sqrt(2 * np.pi)
+    samples = rand(-5.0, 10.0, n=n, pdf=pdf)
+    plt.hist(samples, bins=100), plt.show()  # type: ignore
