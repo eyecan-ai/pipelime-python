a970d125ab7a87c8c4b86f044947f14599d1d7e4
nfioraio-ec <nicola.fioraio@eyecan.ai>
2022-11-17 17:24:45 +0100

    PIPELIME-44 item keys stages tests

--- tests/pipelime/stages/test_item_keys_stages.py
+++ tests/pipelime/stages/test_item_keys_stages.py
@@ -1,3 +1,5 @@
+import pytest
+import typing as t
 from pipelime.sequences import Sample
 from pipelime.items import UnknownItem
 
@@ -79,8 +81,98 @@ class TestKeyStages:
         from pipelime.stages import StageReplaceItem
         from pipelime.items import NpyNumpyItem
 
-        stage = StageReplaceItem(key_item_map={"a": NpyNumpyItem})  # type: ignore
+        stage = StageReplaceItem(key_item_map={"a": NpyNumpyItem, "b": NpyNumpyItem})  # type: ignore
         self._check_target_pred(
             Sample({"a": NpyNumpyItem(10), "c": UnknownItem(20)}),
             stage(Sample({"a": UnknownItem(10), "c": UnknownItem(20)})),
+        )
+
+    def test_duplicate(self):
+        from pipelime.stages import StageDuplicateKey
+        from pipelime.items import NpyNumpyItem
+
+        stage = StageDuplicateKey(source_key="a", copy_to="b")
+        self._check_target_pred(
+            Sample({"a": NpyNumpyItem(10), "b": NpyNumpyItem(10)}),
+            stage(Sample({"a": NpyNumpyItem(10)})),
+        )
+
+        # existing key is not overwritten
+        self._check_target_pred(
+            Sample({"a": NpyNumpyItem(10), "b": UnknownItem(20)}),
+            stage(Sample({"a": NpyNumpyItem(10), "b": UnknownItem(20)})),
+        )
+
+    @pytest.mark.parametrize(
+        ("key_format", "apply_to", "target_sample"),
+        [
+            (
+                "*",
+                None,
+                Sample(
+                    {"a": UnknownItem(10), "b": UnknownItem(20), "c": UnknownItem(20)}
+                ),
+            ),
+            (
+                "new_*",
+                None,
+                Sample(
+                    {
+                        "new_a": UnknownItem(10),
+                        "new_b": UnknownItem(20),
+                        "new_c": UnknownItem(20),
+                    }
+                ),
+            ),
+            (
+                "New",
+                None,
+                Sample(
+                    {
+                        "aNew": UnknownItem(10),
+                        "bNew": UnknownItem(20),
+                        "cNew": UnknownItem(20),
+                    }
+                ),
+            ),
+            (
+                "par_*_tial",
+                ["a", "c"],
+                Sample(
+                    {
+                        "par_a_tial": UnknownItem(10),
+                        "b": UnknownItem(20),
+                        "par_c_tial": UnknownItem(20),
+                    }
+                ),
+            ),
+            (
+                "Str",
+                ["b", "c"],
+                Sample(
+                    {
+                        "a": UnknownItem(10),
+                        "bStr": UnknownItem(20),
+                        "cStr": UnknownItem(20),
+                    }
+                ),
+            ),
+        ],
+    )
+    def test_key_format(
+        self,
+        key_format: str,
+        apply_to: t.Optional[t.Sequence[str]],
+        target_sample: Sample,
+    ):
+        from pipelime.stages import StageKeyFormat
+
+        stage = StageKeyFormat(key_format=key_format, apply_to=apply_to)
+        self._check_target_pred(
+            target_sample,
+            stage(
+                Sample(
+                    {"a": UnknownItem(10), "b": UnknownItem(20), "c": UnknownItem(20)}
+                )
+            ),
         )
