8893067e697a2d93de6788fa3406e6e9322dfec1
nfioraio-ec <nicola.fioraio@eyecan.ai>
2023-03-02 17:24:36 +0100

    fix: AIF-34 close the shared socket only when all refs are gone

--- pipelime/piper/progress/tracker/zmq.py
+++ pipelime/piper/progress/tracker/zmq.py
@@ -1,7 +1,8 @@
 import time
 import zmq
-from typing import Optional
+from typing import Dict, Optional
 from threading import Lock
+import weakref
 
 from pipelime.piper.progress.model import ProgressUpdate
 from pipelime.piper.progress.tracker.base import TrackCallback
@@ -11,37 +12,39 @@ class ZmqTrackCallback(TrackCallback):
     """ZMQ tracker callback"""
 
     _addr: str
-    _socket: Optional[zmq.Socket]
+    _socket: zmq.Socket
+    _finalizer: weakref.finalize
 
-    PROTOTYPES = {}
+    PROTOTYPES: Dict[str, "weakref.ReferenceType[ZmqTrackCallback]"] = {}
     LOCK = Lock()
 
     def __new__(cls, addr: str = "tcp://*:5556"):
         with cls.LOCK:
-            proto = cls.PROTOTYPES.get(addr)
+            proto_ref = cls.PROTOTYPES.get(addr)
+            proto: Optional["ZmqTrackCallback"] = proto_ref() if proto_ref else None
+
             if proto is None:
                 proto = super().__new__(cls)
-                proto._addr = addr
-                proto._socket = None
-                cls.PROTOTYPES[addr] = proto
+
+                # Create the socket
+                proto._socket = zmq.Context().socket(zmq.PUB)
+                proto._socket.bind(addr)
+                proto._finalizer = weakref.finalize(
+                    proto, ZmqTrackCallback.clean_up, proto._socket
+                )
+
+                # Wait for the socket to be ready...
+                # Apparently, this is the only way to do it. I don't know why.
+                time.sleep(1)
+
+                # Save the prototype for future use
+                cls.PROTOTYPES[addr] = weakref.ref(proto)
         return proto
 
     def update(self, prog: ProgressUpdate):
-        if self.socket:
-            topic = prog.op_info.token
-            self.socket.send_multipart([topic.encode(), prog.json().encode()])
+        topic = prog.op_info.token
+        self._socket.send_multipart([topic.encode(), prog.json().encode()])
 
-            if prog.finished:
-                self.socket.close()
-                self._socket = None  # free resources
-
-    @property
-    def socket(self):
-        if not self._socket:
-            self._socket = zmq.Context().socket(zmq.PUB)
-            self._socket.bind(self._addr)
-
-            # Wait for the socket to be ready...
-            # Apparently, this is the only way to do it. I don't know why.
-            time.sleep(1)
-        return self._socket
+    @staticmethod
+    def clean_up(socket: zmq.Socket):
+        socket.close()
